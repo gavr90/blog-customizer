179f5643abee0ace275d05a2388af86cdf1c69d9
Sergei Gavrilov <iamgavr@yandex.ru>
2024-06-05 19:17:49 +0300

    проект выполнен, подготовлен к отправке на ревью

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,35 @@
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import clsx from 'clsx';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
+export type ArrowButtonProps = {
+	open: boolean;
+	handleClick: OnClick;
+};
 
-export const ArrowButton = () => {
+export const ArrowButton = ({ open, handleClick }: ArrowButtonProps) => {
+	const containerMenuStyle = clsx({
+		[styles.container]: true,
+		[styles.container_open]: open, // будет добавлен только когда open === true
+	});
+
+	const arrowMemuStyle = clsx({
+		[styles.arrow]: true,
+		[styles.arrow_open]: open,
+	});
+
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={containerMenuStyle}
+			onClick={handleClick}>
+			<img src={arrow} alt='иконка стрелочки' className={arrowMemuStyle} />
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,133 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Text } from 'components/text';
+import { Select } from '../select/Select';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+import {
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	ArticleStateType,
+} from '../../constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
+import { useState, useRef, useEffect, SyntheticEvent } from 'react';
 
-export const ArticleParamsForm = () => {
+type FormProps = {
+	articleStyle: ArticleStateType;
+	changeStyleHandler: (selected: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	articleStyle,
+	changeStyleHandler,
+}: FormProps) => {
+	// Открытие и закрытие панели через замену стилей
+	const [open, setOpen] = useState(false);
+
+	const menuStyle = clsx({
+		[styles.container]: true,
+		[styles.container_open]: open, // будет добавлен только когда open === true
+	});
+
+	// Состояния отдельных элементов формы
+	const [fontFamily, setFontFamily] = useState(articleStyle.fontFamilyOption);
+	const [fontSize, setFontSize] = useState(articleStyle.fontSizeOption);
+	const [fontColor, setFontColor] = useState(articleStyle.fontColor);
+	const [backgroundColor, setBackgroundColor] = useState(
+		articleStyle.backgroundColor
+	);
+	const [contentWidth, setContentWidth] = useState(articleStyle.contentWidth);
+
+	// // Обработка подтверждения изменений в стили страницы
+	const handleSubmit = (event: SyntheticEvent) => {
+		event.preventDefault();
+		const formState = {
+			fontFamilyOption: fontFamily,
+			fontSizeOption: fontSize,
+			fontColor: fontColor,
+			backgroundColor: backgroundColor,
+			contentWidth: contentWidth,
+		};
+		changeStyleHandler(formState);
+	};
+
+	// Обработка сброса стилей на по умолчанию
+	const handleDefaultState = () => {
+		setFontFamily(defaultArticleState.fontFamilyOption);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBackgroundColor(defaultArticleState.backgroundColor);
+		setContentWidth(defaultArticleState.contentWidth);
+		changeStyleHandler(defaultArticleState);
+	};
+
+	// Закрытие панели по клику снаружи
+	const ref = useRef<HTMLElement | null>(null);
+
+	useEffect(() => {
+		function click(event: MouseEvent) {
+			const target = event.target as HTMLElement;
+			if (ref.current && !ref.current.contains(target)) {
+				setOpen(open);
+			}
+		}
+
+		document.addEventListener('mousedown', click);
+
+		return () => {
+			document.removeEventListener('mousedown', click);
+		};
+	}, []);
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton open={open} handleClick={() => setOpen(!open)} />
+			<aside className={menuStyle} ref={ref}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleDefaultState}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						title={'Шрифт'}
+						options={fontFamilyOptions}
+						selected={fontFamily}
+						onChange={setFontFamily}
+					/>
+					<RadioGroup
+						title={'Размер шрифта'}
+						name='fontsize'
+						options={fontSizeOptions}
+						selected={fontSize}
+						onChange={setFontSize}
+					/>
+					<Select
+						title={'Цвет шрифта'}
+						options={fontColors}
+						selected={fontColor}
+						onChange={setFontColor}
+					/>
+					<Separator />.
+					<Select
+						title={'Цвет фона'}
+						options={backgroundColors}
+						selected={backgroundColor}
+						onChange={setBackgroundColor}
+					/>
+					<Select
+						title={'Ширина контента'}
+						options={contentWidthArr}
+						selected={contentWidth}
+						onChange={setContentWidth}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
